2c208414b6925b3e21d400b149942aecc1ae78cb
ComingS00n <gamesxbow@gmail.com>
2023-12-10 17:38:07 +0700

    up

--- src/components/Sidebar/index.vue
+++ src/components/Sidebar/index.vue
@@ -86,18 +86,8 @@
         <Bars3Icon class="h-6 w-6" aria-hidden="true" />
       </button>
       <div class="flex-1 text-sm font-semibold leading-6 text-gray-900">
-          <!-- {{ this.$route.name }} -->
       </div>
-
-      <!-- <a href="#">
-        <span class="sr-only">Your profile</span>
-        <img
-          class="h-8 w-8 rounded-full bg-gray-50"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
-      </a> -->
-      
+        
       <div class="dropdown dropdown-end hover:bg-gray-50" >
         <div tabindex="0" role="button">
           <div class="flex items-center gap-x-4 text-sm font-semibold leading-6 text-gray-900">
