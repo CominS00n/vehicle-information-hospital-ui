225b173038e196bfc7fcd18337e15c9447b79632
ComingS00n <gamesxbow@gmail.com>
2023-12-10 19:40:35 +0700

    up

---
+++ src/components/Select/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="w-full">
+    <p class="text-slate-500">{{ label }}</p>
+    <Listbox v-model="selectMenu">
+      <div class="relative">
+        <ListboxButton
+          class="relative w-full h-10 border border-slate-400 cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+        >
+          <span :class="`block truncate ${selectMenu === null ? 'text-slate-400' : ''}`">{{
+            selectMenu || placeholder
+          }}</span>
+          <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+            <Icon icon="heroicons-solid:chevron-up-down" />
+          </span>
+        </ListboxButton>
+
+        <transition
+          leave-active-class="transition duration-100 ease-in"
+          leave-from-class="opacity-100"
+          leave-to-class="opacity-0"
+        >
+          <ListboxOptions
+            class="overflow-auto absolute mt-1 max-h-60 w-full rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm z-[999]"
+          >
+            <ListboxOption
+              v-slot="{ active, selected }"
+              v-for="menuList in menuLists"
+              :key="menuList"
+              :value="menuList"
+              as="template"
+            >
+              <li
+                :class="[
+                  active ? 'bg-[#cbf3d9] text-[#099c3d]' : 'text-gray-900',
+                  'relative cursor-default select-none py-2 pl-10 pr-4'
+                ]"
+              >
+                <span :class="[selected ? 'font-medium' : 'font-normal', 'block truncate']">{{
+                  menuList
+                }}</span>
+                <span
+                  v-if="selected"
+                  class="absolute inset-y-0 left-0 flex items-center pl-3 text-[#099c3d]"
+                >
+                  <Icon icon="heroicons-outline:check" />
+                </span>
+              </li>
+            </ListboxOption>
+          </ListboxOptions>
+        </transition>
+      </div>
+    </Listbox>
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/icon/index.vue'
+import { Listbox, ListboxButton, ListboxOptions, ListboxOption } from '@headlessui/vue'
+
+export default {
+  components: {
+    Icon,
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption
+  },
+  data() {
+    return {
+      selectMenu: null
+    }
+  },
+  props: {
+    menuLists: {
+      type: Array,
+      default: []
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: 'Label'
+    }
+  },
+  methods: {
+    emitSelection() {
+      this.$emit('select', this.selectMenu)
+    }
+  },
+  watch: {
+    selectMenu() {
+      this.emitSelection()
+    }
+  }
+}
+</script>
--- src/views/about.vue
+++ src/views/about.vue
@@ -3,21 +3,12 @@
     <h1 class="text-2xl">เพิ่มข้อมูลรถ</h1>
     <hr class="my-4 lg:my-6" />
     <div class="grid lg:grid-cols-3 gap-x-6 gap-y-7">
-      <div class="w-full space-y-6">
-        <textinput label="ทะเบียนรถ" />
-      </div>
-      <div class="w-full space-y-6">
-        <textinput label="ประเภทของรถ" />
-      </div>
-      <div class="w-full space-y-6">
-        <textinput label="เลขไมล์ของรถ" />
-      </div>
-      <div class="w-full space-y-6">
-        <textinput label="วันที่ถ่ายน้ำมันเครื่อง(ครั้งล่าสุด)" />
-      </div>
-      <div class="w-full space-y-6">
-        <textinput label="วันที่ถ่ายน้ำมันเบรก(ครั้งล่าสุด)" />
-      </div>
+      <Select @select="handleSelectTypeCars" :menuLists="typeCars" placeholder="เลือกประเภทของรถ" label="ประเภทของรถ" />
+      <Select @select="handleSelectBrand" :menuLists="brand" placeholder="เลือกแบรนด์ของรถ" label="แบรนด์ของรถ" />
+      <textinput label="หมายเลขทะเบียนรถ" placeholder="กรอกหมายเลขทะเบียนรถ" />
+      <textinput label="เลขไมล์ของรถ" placeholder="กรอกเลขไมล์ของรถ" />
+      <textinput label="วันที่ถ่ายน้ำมันเครื่อง (ครั้งล่าสุด)" placeholder="วันที่ถ่ายน้ำมันเครื่อง" />
+      <textinput label="วันที่ถ่ายน้ำมันเบรก (ครั้งล่าสุด)" placeholder="วันที่ถ่ายน้ำมันเบรก" />
     </div>
     <div class="mt-12 grid gap-y-2 md:flex md:gap-x-4">
       <div class="md:w-52 w-full">
@@ -26,22 +17,79 @@
         >
       </div>
       <div class="w-full">
-        <button @click="addCar" class="btn bg-[#099c3d] text-white hover:bg-[#099c3d] w-full">เพิ่มข้อมูล</button>
+        <button @click="addCar" class="btn bg-[#099c3d] text-white hover:bg-[#099c3d] w-full">
+          เพิ่มข้อมูล
+        </button>
       </div>
     </div>
+    
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import textinput from '@/components/textinput/index.vue'
 import { RouterLink } from 'vue-router'
-import { useToast } from "vue-toastification"
-
+import { useToast } from 'vue-toastification'
+import Select from '@/components/Select/index.vue'
 const toast = useToast()
 
 function addCar() {
-  toast.success("เพิ่มข้อมูลรถเรียบร้อยแล้ว",{
+  toast.success('เพิ่มข้อมูลรถเรียบร้อยแล้ว', {
     timeout: 2000
   })
 }
+
+const typeCars = ref([
+  'รถยนต์',
+  'รถจักรยานยนต์',
+  'รถบรรทุก',
+  'รถตู้',
+  'รถบัส',
+  'รถเก๋ง',
+  'รถตู้ทัวร์'
+])
+
+const brand = ref([
+  'Honda',
+  'Toyota',
+  'Mazda',
+  'Nissan',
+  'Mitsubishi',
+  'Isuzu',
+  'Ford',
+  'Chevrolet',
+  'Mercedes-Benz',
+  'BMW',
+  'Audi',
+  'Volkswagen',
+  'Volvo',
+  'Suzuki',
+  'MG',
+  'Mini',
+  'Porsche',
+  'Lexus',
+  'KIA',
+  'Hyundai',
+  'Jeep',
+  'Land Rover',
+  'Jaguar',
+  'Peugeot',
+  'Subaru',
+  'Ferrari',
+  'Lamborghini',
+  'Rolls-Royce',
+])
+
+const selectTypeCars = ref('')
+const selectBrand = ref('')
+
+function handleSelectTypeCars(value) {
+  selectTypeCars.value = value
+}
+
+function handleSelectBrand(value) {
+  selectBrand.value = value
+}
+
 </script>
